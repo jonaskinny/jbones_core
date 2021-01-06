4358a5f46977b9a070dbfdba7b633b1f0bdcc5de
jonaskinny <jonaskinny@gmail.com>
2021-01-05 23:10:24 -0800

    Add files via upload

---
+++ run_CopyFilesRecursive.bat
@@ -0,0 +1,11 @@
+echo %JAVA_HOME%
+echo off
+call delete_logs
+call setenv_1.8.0_73
+set CLASSPATH=%CLASSPATH%;.\deploy\jbones_core.jar;.\deploy\jbones_core-config.jar;
+echo using classpath ...
+echo %CLASSPATH%
+
+"%JAVA_HOME%\bin\java" -classpath %CLASSPATH% org.jbones.core.io.CopyFilesRecursive
+
+pause
